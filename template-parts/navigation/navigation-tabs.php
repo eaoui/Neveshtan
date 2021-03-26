e61e116a9b2c5e42758c355ff1086352916e44a7
Ehsan Rouhi <ehsanrouhi@live.com>
2021-03-26 22:15:11 +0430

    Change navigation-drawer class-name

--- template-parts/navigation/navigation-tabs.php
+++ template-parts/navigation/navigation-tabs.php
@@ -4,7 +4,7 @@ if (get_theme_mod('display_nav_tabs', true) === true) {
         array(
             'theme_location' => 'nav-tabs',
             'container' => 'ul',
-            'menu_class' => 'navigation-tabs'
+            'menu_class' => 'nav-tabs'
         )
     );
 }
