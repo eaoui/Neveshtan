ce3b4875bd29c8a8ac17faf3c2009f97e0ade52e
Ehsan Rouhi <ehsanrouhi@live.com>
2021-03-27 21:52:30 +0430

    Change navigation-tabs' tag

--- template-parts/navigation/navigation-tabs.php
+++ template-parts/navigation/navigation-tabs.php
@@ -3,7 +3,7 @@ if (get_theme_mod('display_nav_tabs', true) === true) {
     wp_nav_menu(
         array(
             'theme_location' => 'nav-tabs',
-            'container' => 'ul',
+            'container' => 'nav',
             'menu_class' => 'nav-tabs'
         )
     );
