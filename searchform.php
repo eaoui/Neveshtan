755b4468b76d14d97bddedd922da1f5b7b9936fe
Ehsan Rouhi <ehsanrouhi@live.com>
2021-08-05 21:04:03 +0430

    Fix search-box vlue to show search-term

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form method="get" action="<?php echo home_url('/'); ?>" id="searchform" role="search">
-    <input type="search" placeholder="<?php esc_html_e('Search...', 'neveshtan'); ?>" value="<?php get_search_query(); ?>" name="s" id="s">
+    <input type="search" placeholder="<?php esc_html_e('Search...', 'neveshtan'); ?>" value="<?php the_search_query(); ?>" name="s" id="s">
     <input type="submit">
 </form>
